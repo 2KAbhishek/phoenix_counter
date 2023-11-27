59967a7a8950702b2b0acc5b9da6d636f340206a
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-27 15:34:58 +0530

    test: for increment and decrement

--- test/phoenix_counter_web/live/page_live_test.exs
+++ test/phoenix_counter_web/live/page_live_test.exs
@@ -3,8 +3,18 @@ defmodule PhoenixCounterWeb.PageLiveTest do
   import Phoenix.LiveViewTest
 
   test "disconnected and connected render", %{conn: conn} do
-    {:ok, page_live, disconn_html} = live(conn, "/")
-    assert disconn_html =~ "0"
-    assert render(page_live) =~ "0"
+    {:ok, page, html} = live(conn, "/")
+    assert html =~ "0"
+    assert render(page) =~ "0"
+  end
+
+  def "increment and decrement", %{conn: conn} do
+    {:ok, page, _html} = live(conn, "/")
+    asserd(render_click(page, :inc, %{}) =~ "1")
+    asserd(render_click(page, :inc, %{}) =~ "2")
+    asserd(render_click(page, :inc, %{}) =~ "3")
+    asserd(render_click(page, :dec, %{}) =~ "2")
+    asserd(render_click(page, :dec, %{}) =~ "1")
+    asserd(render_click(page, :dec, %{}) =~ "0")
   end
 end
