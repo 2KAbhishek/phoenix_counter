56bdf6a26d12ecc0ba0c384df0c399ab85bfd43c
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-27 16:22:52 +0530

    feat: add interactivity!

--- lib/phoenix_counter_web/live/counter.ex
+++ lib/phoenix_counter_web/live/counter.ex
@@ -1,3 +1,19 @@
 defmodule PhoenixCounterWeb.Counter do
   use PhoenixCounterWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, count: 0)}
+  end
+
+  def handle_event("increment", _, socket) do
+    {:noreply, assign(socket, count: socket.assigns.count + 1)}
+  end
+
+  def handle_event("decrement", _, socket) do
+    {:noreply, assign(socket, count: socket.assigns.count - 1)}
+  end
+
+  def handle_event("clear", _, socket) do
+    {:noreply, assign(socket, count: 0)}
+  end
 end
